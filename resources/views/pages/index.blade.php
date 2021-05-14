a5fbdd9af9c1e2d6adebe475005c4ad340e65d9e
Vincentsarsah <69487577+Vincentsarsah@users.noreply.github.com>
2021-05-14 17:22:05 +0000

    first commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="jumbotron text-center">
+     <h1>THE BLOG</h1>
+     <h5>This is a mini project by Eric,Vincent and George</h5>
+     <br/>
+     <hr/>
+     <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
+    </div>
+@endsection
